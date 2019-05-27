0a83a2c62433cd88047fd2d221431951459874d3
TungYao <1922527051@qq.com>
2019-05-27 15:02:49 +0800

    add ottofs

---
+++ ottofs/ottofs.c
@@ -0,0 +1,7 @@
+#include "ottofs.h"
+
+int main(){
+    printf("HELLO  WORLD");
+
+    return  0;
+}
---
+++ ottofs/ottofs.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#ifndef HEADER_FILE
+#define HEADER_FILE
+FILE *fp;
+
+
+#endif
